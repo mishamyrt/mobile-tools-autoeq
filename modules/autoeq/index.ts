d96d26a82bdedd77de35faf8b6cc01a61d3275b2
mishamyrt <misha@myrt.co>
2021-12-05 23:29:48 +0400

    🙃

--- modules/autoeq/index.ts
+++ modules/autoeq/index.ts
@@ -1,7 +1,9 @@
 import { GainData } from '../audio'
 
-function mapToFixed (v: number[]): string[] {
-  return v.map(i => i.toFixed(2))
+const TABLE_HEADERS = ['frequency', 'raw']
+
+function numbersToFixed (items: number[]): string[] {
+  return items.map(i => i.toFixed(2))
 }
 
 function joinCSVLine (data: any[]) {
@@ -9,8 +11,8 @@ function joinCSVLine (data: any[]) {
 }
 
 export function formatMeasurement (data: GainData) {
-  const tuples = data.map(mapToFixed)
-  return [['frequency', 'raw'], ...tuples]
+  const tuples = data.map(numbersToFixed)
+  return [TABLE_HEADERS, ...tuples]
     .map(joinCSVLine)
     .join('\n')
 }
